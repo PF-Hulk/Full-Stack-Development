87071c9aa51be5485cdb3c038b057e2bcd9bf3b2
PF-Hulk <chris72092@aol.com>
2026-07-13 21:57:02 -0400

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,15 @@
+const fs = require('fs');
+
+const trips = JSON.parse(
+  fs.readFileSync('./data/trips.json', 'utf8')
+);
+
 /* GET travel page */
 const travel = (req, res) => {
-  res.render('travel', { title: 'Travlr Getaways' });
+  res.render('travel', {
+    title: 'Travlr Getaways',
+    trips
+  });
 };
 
 module.exports = {
